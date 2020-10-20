6bf146e1756ce46c5a2e11a013955158e1c81256
Alex-J21 <rogozhnikov19976@gmail.com>
2020-10-20 22:06:57 +0300

    тз №10 резиновая верстка

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
     $(".owl-carousel").owlCarousel();
 });
+const closePopup = document.getElementById('popup-close');
+const popup = document.getElementById('popup');
+
+closePopup.onclick = function(event){
+    event.preventDefault();
+    popup.classList.add('hidden')
+}
